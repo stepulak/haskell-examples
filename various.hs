fc959d365eb0656282d4253ab8c64244336cd7e1
Štěpán Trčka <savage2@email.cz>
2016-11-11 12:53:39 +0100

    Add files via upload

---
+++ various.hs
@@ -0,0 +1,55 @@
+median :: [a] -> Maybe a
+median [] = Nothing
+median [x] = Just (x)
+median [x,y] = Just (x)
+median s = median $ (tail . init) s
+
+isVowel :: Char -> Bool
+isVowel x = x `elem` "aeiouyAEIOUY"
+
+filterVowels :: String -> String
+filterVowels = filter (isVowel)
+
+isLower :: Char -> Bool
+isLower x = x `elem` ['a'..'z']
+
+isUpper :: Char -> Bool
+isUpper x = x `elem` ['A'..'Z']
+
+toUpper :: Char -> Char
+toUpper x
+    | isLower x = (toEnum $ fromEnum x + (fromEnum 'A' - fromEnum 'a')) :: Char
+    | otherwise = x
+
+toUpperStr :: String -> String
+toUpperStr = map (toUpper)
+
+toLower :: Char -> Char
+toLower x
+    | isUpper x = (toEnum $ fromEnum x - (fromEnum 'A' - fromEnum 'a')) :: Char
+    | otherwise = x
+    
+toLowerStr :: String -> String
+toLowerStr = map (toLower)
+
+takeN :: Int -> [a] -> [a]
+takeN _ [] = []
+takeN n (x:s)
+    | n <= 0 = []
+    | otherwise = x : takeN (n-1) s
+    
+getPrimes :: [Int]
+getPrimes = filter [2..]
+    where
+     filter (x:s) = x : [y | y <- filter s, y `mod` x /= 0]
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:s) = qsort [y | y <- s, y <= x] ++ [x] ++ qsort [y | y <- s, y > x]
+
+insertSort :: (Ord a) => [a] -> [a]
+insertSort [] = []
+insertSort s = foldr (insert) [] s
+    where
+     insert x [] = [x]
+     insert x (y:s) = if x > y then y : insert x s else x : y : s
